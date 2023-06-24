042796f6245151e8d51603fda602ffb8da4250b6
simbankenya <pharesmuruthi@gmail.com>
2023-06-24 21:40:10 +0300

    setting up input fields

--- components/AppTextInput.js
+++ components/AppTextInput.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { TextInput, View, StyleSheet } from 'react-native'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
-function AppTextInput() {
-  return <div>AppTextInput</div>
+function AppTextInput({ icon, width = '100%', ...otherProps }) {
+  return (
+    <View style={[styles.container, { width }]}>
+      {icon && (
+        <MaterialCommunityIcons name={icon} size={20} color="" style="" />
+      )}
+      <TextInput placeholderTextColor="" style="" {...otherProps}></TextInput>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'gray',
+    borderRadius: 25,
+    flexDirection: 'row',
+    padding: 15,
+    marginVertical: 10,
+  },
+})
 
 export default AppTextInput
--- screens/Login.js
+++ screens/Login.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import Screen from '../components/Screen'
 
 function Login() {
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <Screen>
+      <View>
+        <Text>Login</Text>
+      </View>
+    </Screen>
   )
 }
 
